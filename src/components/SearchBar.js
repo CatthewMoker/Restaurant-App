85c37aa8706dd8a2598f809511c30703f69fad6b
Coker <mattacoker@gmail.com>
2020-12-13 12:04:54 -0500

    set up state for Search Bar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,19 +1,41 @@
 import React from 'react';
+import { FontAwesome } from '@expo/vector-icons';
 import { Text, View, StyleSheet } from 'react-native';
+import { TextInput } from 'react-native-gesture-handler';
 
-const SearchBar = () => {
+const SearchBar = ({ term, onTermChange, onTermSubmit }) => {
     return (
-        <View style={styles.background}>
-            <Text>Search Bar</Text>
+        <View style={styles.backgroundStyle}>
+            <FontAwesome style={styles.iconStyle} name="search" size={24} color="black" />
+            <TextInput 
+            autoCapitalize="none"
+            autoCorrect={false}
+            style={styles.inputStyle} 
+            placeholder='Search'
+            value={term}
+            onChangeText={onTermChange}
+            onEndEditing={onTermSubmit}
+            />
         </View>
     );
 };
 
 const styles = StyleSheet.create({
-    background: {
+    backgroundStyle: {
+        marginTop: 10,
         backgroundColor: '#F0EEEF',
         height: 50,
         borderRadius: 5,
+        marginHorizontal: 15,
+        flexDirection:'row'
+    },
+    inputStyle: {
+        borderColor: 'black',
+        fontSize: 18
+    },
+    iconStyle: {
+        fontSize: 35,
+        alignSelf: 'center',
         marginHorizontal: 15
     }
 });
