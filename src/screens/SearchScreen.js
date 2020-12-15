e9041c01016f69a006b5b9d9ab2476c285281538
Matthew Coker <mattacoker@gmail.com>
2020-12-15 18:00:57 -0500

    added photos for individual businesses and restaurant app is completed

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Text, View, StyleSheet, ScrollView } from 'react-native';
+import { Text, View, StyleSheet, ScrollView, ImagePropTypes } from 'react-native';
 import yelp from '../api/yelp';
 import SearchBar from '../components/SearchBar';
 import useResults from '../hooks/useResults';
@@ -10,12 +10,7 @@ const SearchScreen = () => {
     const [term, setTerm] = useState('');
     const [searchApi, results, errorMessage] = useResults();
 
-    // Call searchAPI when component
-    // is first rendered. BAD CODE!
-    // searchApi('pasta')
-    useEffect(() => {
-        searchApi('pasta');
-    }, [])
+
 
     const filterResultsByPrice = (price) => {
         // === '$'|| '$$' || '$$$'
@@ -24,21 +19,34 @@ const SearchScreen = () => {
         });
     };
 
+    // Call searchAPI when component
+    // is first rendered. BAD CODE!
+    // searchApi('pasta')
+    useEffect(() => {
+        searchApi('pasta');
+    }, [])
+
     return (
-        <View style={{ flex: 1 }}>
+        <>
             <SearchBar 
             term={term} 
             onTermChange={setTerm} 
             onTermSubmit={() => searchApi(term)}
             />
             {errorMessage ? <Text>{errorMessage}</Text> : null}
-            <Text>We have found {results.length} results</Text>
+            {/* <Text>We have found {results.length} results</Text> */}
             <ScrollView>
-                <ResultsList results={filterResultsByPrice('$')} title="Cost Effective" />
-                <ResultsList results={filterResultsByPrice('$$')} title="Bit Pricier" />
-                <ResultsList results={filterResultsByPrice('$$$')} title="Big Spender" />
+                <ResultsList 
+                results={filterResultsByPrice('$')} 
+                title="Cost Effective" />
+                <ResultsList 
+                results={filterResultsByPrice('$$')} 
+                title="Bit Pricier" />
+                <ResultsList 
+                results={filterResultsByPrice('$$$')} 
+                title="Big Spender" />
             </ScrollView>
-        </View>
+        </>
     );
 };
 
